75b8b834e25c30cddb99dae4830ac076b387fd1a
Peter Freshman <peter.freshman@nuance.com>
2024-10-10 09:24:20 -0400

    add versioning

---
+++ version.cs
@@ -0,0 +1,10 @@
+namespace SpeechEnabledCoPilot;
+
+/// <summary>
+/// Represents the version of the application.
+/// </summary>
+public class Version {
+
+    // The version of the application.
+    public static readonly string version = "0.0.1";
+}
