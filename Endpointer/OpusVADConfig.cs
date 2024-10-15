a9927463b6ab147fbde6f81cf285b3907c911dca
Peter Freshman <peter.freshman@nuance.com>
2024-10-15 15:05:51 -0400

    document opusvad. refactor opusvad config.

--- Endpointer/OpusVADConfig.cs
+++ Endpointer/OpusVADConfig.cs
@@ -1,13 +1,20 @@
 namespace SpeechEnabledCoPilot.Endpointer
 {
+    /// <summary>
+    /// Configuration for the Opus VAD.
+    /// </summary>
     public class OpusVADConfig {
 
+        /// <summary>
+        /// Default values for the Opus VAD configuration.
+        /// </summary>
         public class Defaults {
             public static readonly int FRAME_SIZE_MS = 20;
             public static readonly int LEADING_SILENCE_WINDOW_MS = 120;
             public static readonly int SOS_WINDOW_MS = 220;
             public static readonly int EOS_WINDOW_MS = 1100;
-            public static readonly int DEFAULT_COMPLEXITY = 3;
+            public static readonly int COMPLEXITY = 3;
+            public static readonly int BIT_RATE_TYPE = OpusVAD.OPUSVAD_BIT_RATE_TYPE_CVBR;
             public static readonly int SENSITIVITY = 20;
             public static readonly int AUDIO_BUFFER_SIZE = 50 * 2;
         }
@@ -16,7 +23,8 @@ namespace SpeechEnabledCoPilot.Endpointer
         public readonly int LEADING_SILENCE_WINDOW_MS = Defaults.LEADING_SILENCE_WINDOW_MS;
         public readonly int SOS_WINDOW_MS = Defaults.SOS_WINDOW_MS;
         public readonly int EOS_WINDOW_MS = Defaults.EOS_WINDOW_MS;
-        public readonly int DEFAULT_COMPLEXITY = Defaults.DEFAULT_COMPLEXITY;
+        public readonly int COMPLEXITY = Defaults.COMPLEXITY;
+        public readonly int BIT_RATE_TYPE = Defaults.BIT_RATE_TYPE;
         public readonly int SENSITIVITY = Defaults.SENSITIVITY;
         public readonly int AUDIO_BUFFER_SIZE = Defaults.AUDIO_BUFFER_SIZE;
     }    
