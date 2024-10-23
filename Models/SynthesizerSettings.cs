ee8ba729d028885de0490d653ad56c0d02d0b2a6
Peter Freshman <peter.freshman@nuance.com>
2024-10-23 12:37:34 -0400

    fix lost changes from a previous merge

--- Models/SynthesizerSettings.cs
+++ Models/SynthesizerSettings.cs
@@ -15,7 +15,7 @@ namespace SpeechEnabledTvClient.Models
             public const string SubscriptionKey = "YOUR_SUBSCRIPTION_KEY";
             public const string ServiceRegion = "eastus";
             public const string VoiceName = "en-US-AvaMultilingualNeural";
-            public const string SpeechSynthesisOutputFormat = "Raw48KHz16BitMonoPcm";
+            public const string SpeechSynthesisOutputFormat = "Raw22050Hz16BitMonoPcm";
             public const string DestAudioType = "speaker"; // speaker or file
             public const string DestAudioPath = "./audio"; // path to either a folder containing audio or a specific audio file
         }
