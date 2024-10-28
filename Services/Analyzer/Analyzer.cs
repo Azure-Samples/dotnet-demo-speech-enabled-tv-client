2fc59001e11f6bd1b8b3e86b98cea9bb9078f7ac
Peter Freshman <peter.freshman@nuance.com>
2024-10-28 12:10:29 -0400

    Add support to analyzer for 2nd pass prompt completions

--- Services/Analyzer/Analyzer.cs
+++ Services/Analyzer/Analyzer.cs
@@ -24,6 +24,7 @@ namespace SpeechEnabledTvClient.Services.Analyzer
 
         AnalyzerSettings settings = AppSettings.AnalyzerSettings();
         private readonly ConversationAnalysisClient client;
+        private readonly PromptCompletion? promptCompletion;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Analyzer"/> class.
@@ -42,6 +43,12 @@ namespace SpeechEnabledTvClient.Services.Analyzer
             }
             AzureKeyCredential credential = new AzureKeyCredential(settings.CluKey);
             client = new ConversationAnalysisClient(uri, credential);
+
+            // Initialize the prompt completion service if enabled.
+            if (settings.Enable2ndPassCompletion)
+            {
+                promptCompletion = new PromptCompletion(logger, new SpeechEnabledTvClient.Monitoring.Monitor(AppSettings.LoadAppSettings(new string[] { })));
+            }
         }
 
         /// <summary>
@@ -79,6 +86,124 @@ namespace SpeechEnabledTvClient.Services.Analyzer
         }
 
         /// <summary>
+        /// Builds a response from an Analyzer service error response.
+        /// </summary>
+        /// <param name="response">The response from the Analyzer service.</param>
+        /// <returns>AnalyzerResponse</returns>
+        private AnalyzerResponse BuildResponseFromError(Response? response)
+        {
+            // Read the error response from the content stream.
+            ErrorResponse errorResponse = ErrorResponse.FromContentStream(response?.ContentStream);
+
+            // monitor and log error details
+            monitor.IncrementRequests("Error");
+            
+            activity?.SetTag("Disposition", "Error");
+            activity?.SetTag("ErrorCode", errorResponse.content.error.code);
+            activity?.SetTag("ErrorMessage", errorResponse.content.error.message);
+
+            // Return the error response.
+            return new AnalyzerResponse(errorResponse);
+        }
+
+        /// <summary>
+        /// Processes the calendarx entity.
+        /// </summary>
+        /// <param name="entityText">The entity text to process.</param>
+        /// <param name="sessionId">The session ID.</param>
+        /// <param name="requestId">The request ID.</param>
+        /// <returns>NuanceCalendarOption</returns>
+        /// <remarks>
+        /// This method provides a simple example of applying 2nd pass prompt completion against an entity
+        /// that, with legancy Nuance NLUaaS, provided a more detailed breakdown of the entity.
+        /// </remarks>
+        private NuanceCalendarOption ProcessCalendarXEntity(string entityText, string? sessionId, int requestId)
+        {
+            // safety check...
+            if (promptCompletion == null)
+            {
+                return new NuanceCalendarOption();
+            }
+
+            // Use the prompt completion service to get the calendarx entity details.
+            PromptOutput promptOutput = promptCompletion.GetPromptCompletion("calendarx.json", entityText, sessionId, requestId).Result;
+            if (!promptOutput.IsError)
+            {
+                // Deserialize the calendarx entity details.
+                var calData = JsonSerializer.Deserialize<NuanceCalendarOption>(promptOutput.ResponseData.Choices[0].Message.Content);
+                if (calData != null)
+                {
+                    return calData;
+                }
+            } else {
+                logger.LogError($"PromptOutput: {promptOutput.ReasonPhrase}");
+            }
+            return new NuanceCalendarOption();
+        }
+
+        /// <summary>
+        /// Processes the entities.
+        /// </summary>
+        /// <param name="entities">The entities to process.</param>
+        /// <param name="sessionId">The session ID.</param>
+        /// <param name="requestId">The request ID.</param>
+        /// <returns>Entity[]</returns>
+        /// <remarks>
+        /// This method loops through the entities and applies additional processing to the calendarx entity.
+        /// </remarks>
+        private Entity[] ProcessEntities(Entity[] entities, string? sessionId, int requestId)
+        {
+            // ignore if 2nd pass completion is not enabled
+            if (!settings.Enable2ndPassCompletion)
+            {
+                return entities;
+            }
+
+            // for each entity, check for and process calendarx entities
+            int entityCount = 0;
+            foreach (var entity in entities)
+            {
+                if (entity.category == "nuance_CALENDARX")
+                {
+                    logger.LogInformation($"Found nuance_CALENDARX entity: {entity.text}");
+                    entities[entityCount].nuance_CALENDARX = ProcessCalendarXEntity(entity.text, sessionId, requestId);
+                }
+                entityCount++;
+            }
+            return entities;
+        }
+
+        /// <summary>
+        /// Builds a response from an Analyzer service successful response.
+        /// </summary>
+        /// <param name="response">The response from the Analyzer service.</param>
+        /// <param name="sessionId">The session ID.</param>
+        /// <param name="requestId">The request ID.</param>
+        /// <returns>AnalyzerResponse</returns>
+        private AnalyzerResponse BuildResponseFromResponse(Response response, string? sessionId, int requestId)
+        {
+            // monitor some stuff
+            monitor.IncrementRequests("Success");
+            activity?.SetTag("Disposition", "Success");
+
+            // Deserialize the response content.
+            Interpretation interpration = Interpretation.FromJson(response.Content.ToString());
+            activity?.SetTag("Result", JsonSerializer.Serialize(interpration.result));
+
+            // Log the interpretation.
+            if (sessionId != null) {
+                logger.LogInformation($"[{sessionId}.{requestId}] Interpretation: {interpration.result.prediction.topIntent}");
+            } else {
+                logger.LogInformation($"Interpretation: {interpration.result.prediction.topIntent}");
+            }
+
+            // Process the entities.
+            interpration.result.prediction.entities = ProcessEntities(interpration.result.prediction.entities, sessionId, requestId);
+            
+            return new AnalyzerResponse(interpration);
+        }
+
+        /// <summary>
         /// Analyzes the input.
         /// </summary>
         /// <param name="input"></param>
@@ -99,45 +224,22 @@ namespace SpeechEnabledTvClient.Services.Analyzer
                 DateTime start = DateTime.Now;
                 Response? response = null;
                 long latency = 0;
-                // Use the client to send the input to the Azure CLU service for analysis.
+
                 try
                 {
+                    // Use the client to send the input to the Azure CLU service for analysis.
                     response = client.AnalyzeConversation(createInput(input));
                     latency = (long)(DateTime.Now - start).TotalMilliseconds;
 
+                    // Check for an error response.
                     if (response.IsError) {
-                        ErrorResponse errorResponse = ErrorResponse.FromContentStream(response?.ContentStream);
-
-                        monitor.IncrementRequests("Error");
                         monitor.RecordLatency(latency, "Error");
-                        
-                        activity?.SetTag("Disposition", "Error");
-                        activity?.SetTag("Status", response?.Status.ToString());
-                        activity?.SetTag("Reason", response?.ReasonPhrase);
-                        activity?.SetTag("ErrorCode", errorResponse.content.error.code);
-                        activity?.SetTag("ErrorMessage", errorResponse.content.error.message);
-
-                        return new AnalyzerResponse(errorResponse);
+                        return BuildResponseFromError(response);
                     }
 
-                    if (!response.IsError) {
-                        monitor.IncrementRequests("Success");
-                        monitor.RecordLatency(latency, "Success");
-                        activity?.SetTag("Disposition", "Success");
-
-                        Interpretation interpration = Interpretation.FromJson(response.Content.ToString());
-                        activity?.SetTag("Result", JsonSerializer.Serialize(interpration.result));
-
-                        if (sessionId != null) {
-                            logger.LogInformation($"[{sessionId}.{requestId}] Interpretation: {0}", interpration.result.prediction.topIntent);
-                        } else {
-                            logger.LogInformation("Interpretation: {0}", interpration.result.prediction.topIntent);
-                        }
-                        
-                        return new AnalyzerResponse(interpration);
-                    }
-
-                    return new AnalyzerResponse();
+                    // Build the response from the successful response.
+                    monitor.RecordLatency(latency, "Success");
+                    return BuildResponseFromResponse(response, sessionId, requestId);
                 }
                 catch (System.Exception ex)
                 {
@@ -151,8 +253,6 @@ namespace SpeechEnabledTvClient.Services.Analyzer
                         monitor.RecordLatency(latency, "Error");
 
                         activity?.SetTag("Disposition", "Error");
-                        activity?.SetTag("Status", response?.Status.ToString());
-                        activity?.SetTag("Reason", response?.ReasonPhrase);
                         activity?.SetTag("ErrorCode", errorResponse.content.error.code);
                         activity?.SetTag("ErrorMessage", errorResponse.content.error.message);
                         
@@ -162,7 +262,7 @@ namespace SpeechEnabledTvClient.Services.Analyzer
                     return new AnalyzerResponse();
                 } 
                 finally {
-                    activity?.SetTag("RequestID", response?.ClientRequestId);
+                    activity?.SetTag("ClientRequestID", response?.ClientRequestId);
                     activity?.SetTag("Status", response?.Status.ToString());
                     activity?.SetTag("Reason", response?.ReasonPhrase);
                 }
